69c5a574748813d08cf52d02fc56f8ba61865c6e
chifius <chifius@mail.ru>
2020-09-20 22:30:56 +0300

    make output in to dashboard some cards of movies

--- moviecatalog/src/app/moviedashboard/moviedashboard.component.ts
+++ moviecatalog/src/app/moviedashboard/moviedashboard.component.ts
@@ -1,15 +1,18 @@
+import { MoviesService } from './../movies.service';
+import { Movie } from './../movie.interface';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-moviedashboard',
   templateUrl: './moviedashboard.component.html',
-  styleUrls: ['./moviedashboard.component.css']
+  styleUrls: ['./moviedashboard.component.css'],
 })
 export class MoviedashboardComponent implements OnInit {
+  movies: Movie[] = [];
 
-  constructor() { }
+  constructor(private moviesService: MoviesService) {}
 
   ngOnInit(): void {
+    this.movies = this.moviesService.getMovies();
   }
-
 }
