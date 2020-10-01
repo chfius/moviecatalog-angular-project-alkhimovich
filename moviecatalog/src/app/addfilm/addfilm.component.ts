f29757c2beb0a1437b1a86a28d34541753ef322b
chifius <chifius@mail.ru>
2020-10-01 15:11:31 +0300

    update form

--- moviecatalog/src/app/addfilm/addfilm.component.ts
+++ moviecatalog/src/app/addfilm/addfilm.component.ts
@@ -1,15 +1,35 @@
+import { genres } from './../models/genres';
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-addfilm',
   templateUrl: './addfilm.component.html',
-  styleUrls: ['./addfilm.component.css']
+  styleUrls: ['./addfilm.component.css'],
 })
 export class AddfilmComponent implements OnInit {
+  genres: string[] = [];
 
-  constructor() { }
+  newFilm = new FormGroup({
+    poster: new FormControl(null),
+    title: new FormControl(null),
+    year: new FormControl(null),
+    genre: new FormControl(null),
+    isWatched: new FormControl(null),
+    country: new FormControl(null),
+    director: new FormControl(null),
+    actors: new FormControl(null),
+    rate: new FormControl(null),
+    desription: new FormControl(null),
+  });
 
-  ngOnInit(): void {
+  onSubmit(form: NgForm): void {
+    console.log(form.value);
   }
 
+  constructor() {}
+
+  ngOnInit(): void {
+    genres.forEach((item) => this.genres.push(item));
+  }
 }
